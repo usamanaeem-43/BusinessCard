c49a4d83aa262ec46a6f56688a2e0b1755869bbb
Usama Naeem <usamanaeem43@gmail.com>
2023-09-06 09:48:39 +0500

    Business Card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const App = () => {
+  return (
+    <div className="main-div">
+      <img src="myPhoto.JPG" className="myImage"></img>
+      <div className="intro-div">
+        <h3>M. Usama Naeem</h3>
+        <h5>Web Developer</h5>
+        <p>(usamanaeem43@gmail.com)</p>
+      </div>
+      <div className="button-div">
+        <button className="btn btn-primary">Email Me</button>
+        <button id="myUpwork" className="btn btn-primary">
+          My Upwork
+        </button>
+        <button id="myFiverr" className="btn btn-primary">
+          My Fiverr
+        </button>
+      </div>
+      <h5>About</h5>
+      <p>
+        I am an Electrical Engineer turned Programmer. For more than 4 Years, I
+        have been developing Software for Small to Medium Level Businesses using
+        VBA, AppScript and Javascript. Currently I am Learning React so I can
+        create Scalable, High Performance Websites
+      </p>
+      <h5>Interests</h5>
+      <p>Programming, History, Music, Seasons and Jogging</p>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <div>
     <App />
-  </React.StrictMode>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
